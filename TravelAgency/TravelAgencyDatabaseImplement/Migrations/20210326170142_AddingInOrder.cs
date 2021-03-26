25e742bfe7716bcf4f896892b4fa2ea08fb657d6
Ксения Савкина <ksenia.savkina2001@gmail.com>
2021-03-26 21:04:38 +0400

    bug fixes

---
+++ TravelAgency/TravelAgencyDatabaseImplement/Migrations/20210326170142_AddingInOrder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAgencyDatabaseImplement.Migrations
+{
+    public partial class AddingInOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
