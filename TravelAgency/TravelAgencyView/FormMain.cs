be5ae1ad9be7137aebf6fdbbe8c772edc649d225
ksenia-savkina <ksenia.savkina2001@gmail.com>
2021-04-17 17:57:54 +0400

    fix

--- TravelAgency/TravelAgencyView/FormMain.cs
+++ TravelAgency/TravelAgencyView/FormMain.cs
@@ -38,6 +38,7 @@ namespace TravelAgencyView
                     dataGridView.Columns[0].Visible = false;
                     dataGridView.Columns[1].Visible = false;
                     dataGridView.Columns[2].Visible = false;
+                    dataGridView.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 }
             }
             catch (Exception ex)
